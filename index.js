85d8a1a9f3eb89700f330c38f0ed3ed5b4d6198d
Frank Lee <plat4m@outlook.com>
2022-03-19 18:14:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const cards = [];
+
+function writeCards(name) {
+  for(let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        }
+        return cards;
+    }
+
+
+function countDown() {
+
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
